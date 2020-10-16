5d467a0ce5b0599446d6e56876e73d55b8948ef4
Samuel Vandamme <samuel@sava.be>
2020-10-16 12:36:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SCOM Management pack tools
+
+# export.py
+
+Run with `python3 export.py [path to file]`
+After the script exits succesfully you will find the Management packs scripts in the `export` directory.
+
+## Example
+
+`python3 export.py data/Microsoft.Windows.Server.AD.2016.Monitoring.xml`
---
+++ export.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#
+# Takes a path as argument to an unsealed management XML file and exports all the scripts to the output directory
+#
+
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import sys
+import os
+
+filename = sys.argv[1]
+print("Parsing file %s" % (filename))
+DOMTree = xml.dom.minidom.parse(filename)
+collection = DOMTree.documentElement
+datasources = collection.getElementsByTagName("DataSourceModuleType")
+
+print("Creating output directory")
+output = os.path.join(os.getcwd(), "export/%s" % (os.path.basename(filename)))
+try:
+    os.mkdir(output)
+except OSError as error:
+    print(error)
+
+print("Found %s datasources" % (len(datasources)))
+for datasource in datasources:
+    name = datasource.getAttribute("ID")
+    print(name)
+    files = datasource.getElementsByTagName("File")
+    for idx, file in enumerate(files):
+        filename = file.getElementsByTagName("Name")[0].childNodes[0].data
+        content = file.getElementsByTagName("Contents")[0].childNodes[0].data
+        f = open(os.path.join(output, filename), "a")
+        f.write(content)
+        f.close()
